cb612128cf612d13a0938b8bf333a0bc9b4c6c51
SkalskiP <piotr.skalski92@gmail.com>
2019-04-14 00:34:08 +0200

    Work on library project started

---
+++ lab6/library/ejb-api/src/main/java/dto/BookDTO.java
@@ -0,0 +1,127 @@
+package dto;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity()
+@Table(name = "books")
+@Access(AccessType.FIELD)
+public class BookDTO implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "author_name")
+    private String authorName;
+
+    @Column(name = "author_surname")
+    private String authorSurname;
+
+    @Column(name = "category", nullable = false)
+    private String category;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "pages", nullable = false)
+    private Integer pages;
+
+    @Column(name = "publisher")
+    private String publisher;
+
+    @Column(name = "isbn", nullable = false)
+    private String isbn;
+
+    @Column(name = "count", nullable = false)
+    private Integer count;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getAuthorSurname() {
+        return authorSurname;
+    }
+
+    public void setAuthorSurname(String authorSurname) {
+        this.authorSurname = authorSurname;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return this.authorName + " " + this.authorSurname + " " + this.title + " " + this.category + " " + this.price;
+    }
+}
