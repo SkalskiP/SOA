ef7d66fd9daef0d40a0aeb8c13588052dbcb87de
SkalskiP <piotr.skalski92@gmail.com>
2019-05-16 20:34:50 +0200

    currency soap service up

--- lab9/currency/soap_service/src/main/java/service/CurrencyServiceImpl.java
+++ lab9/currency/soap_service/src/main/java/service/CurrencyServiceImpl.java
@@ -3,14 +3,20 @@ package service;
 import domain.Currency;
 import util.CurrencyCalculator;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
 public class CurrencyServiceImpl implements CurrencyService {
     @Override
+    @WebMethod
     public Double getExchangeRateToPLN(String currencyString) {
         Currency currency = CurrencyCalculator.convertStringCurrency(currencyString);
         return CurrencyCalculator.getExchangeRateToPLN(currency);
     }
 
     @Override
+    @WebMethod
     public Double exchangeToPLN(String currencyString, Double amount) {
         Currency currency = CurrencyCalculator.convertStringCurrency(currencyString);
         return CurrencyCalculator.exchangeToPLN(currency, amount);
